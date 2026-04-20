dca8a7aa310f61d2aac9c6905190054ed18f7e8b
vuongnd9 <nguyendinhvuong.act@gmail.com>
2026-04-20 22:52:14 +0700

    Add serilog for API

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
+using Serilog;
 using WeatherAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(
+    path: "Logs/log-.txt",
+    rollingInterval: RollingInterval.Day,
+    retainedFileCountLimit: 7
+).CreateLogger();
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddHttpClient<IWeatherService, WeatherService>();
+builder.Host.UseSerilog();
+
+//Build App
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- Services/WeatherService.cs
+++ Services/WeatherService.cs
@@ -5,36 +5,43 @@ using WeatherAPI.Models;
 
 namespace WeatherAPI.Services
 {
-  public class WeatherService(HttpClient httpClient, IConfiguration configuration) : IWeatherService
+  public class WeatherService(HttpClient httpClient, IConfiguration configuration, ILogger<WeatherService> logger) : IWeatherService
   {
     private readonly HttpClient _httpClient = httpClient;
     private readonly string _myKey = configuration["WeatherApiKey"] ?? throw new InvalidOperationException("Has no weather api key");
+
+    private readonly ILogger<WeatherService> _logger = logger;
     private const string API_BASE = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
     public async Task<WeatherInfo> GetWeatherAsync(string city)
     {
+      _logger.LogInformation("Fetching weather for city: {City}", city);
       try
       {
         var url = $"{API_BASE}/{city}?key={_myKey}";
         var response = await _httpClient.GetAsync(url);
         if (!response.IsSuccessStatusCode)
         {
+          _logger.LogWarning("Weather API return {StatusCode} for city {City}", response.StatusCode, city);
           throw new WeatherException($"Weather API return {response.StatusCode}");
         }
         var content = await response.Content.ReadAsStringAsync();
         var weatherResponse = JsonSerializer.Deserialize<WeatherResponse>(content) ?? throw new WeatherException("No information retrieve");
-
+        _logger.LogInformation("Weather data retrieve successfully for {City}", city);
         return ParseWeatherReponse(weatherResponse);
       }
       catch (TaskCanceledException ex)
       {
+        _logger.LogError(ex, "Timeout when retrieve weather for {City}", city);
         throw new WeatherException("Weather API Timeout", ex);
       }
       catch (HttpRequestException ex)
       {
+        _logger.LogError(ex, "Network error when calling Weather API when retrieve weather for {City}", city);
         throw new WeatherException("Network error when calling Weather API", ex);
       }
       catch (JsonException ex)
       {
+        _logger.LogError(ex, "Fail to deserialize when retrieve weather for {City}", city);
         throw new WeatherException("Fail to deserialize", ex);
       }
     }
